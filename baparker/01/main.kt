f45f447157596bfc2ebfab6c32c7b4c7efc72dc8
Brian Parker <BParker222@gmail.com>
2021-12-02 22:08:43 -0500

    01 solution with variable window size

--- baparker/01/main.kt
+++ baparker/01/main.kt
@@ -1,17 +1,21 @@
 import java.io.File
 
-fun main() {
+fun main(args: Array<String>) {
+  var windowSize = 1
+  if (args.size > 0) {
+    windowSize = args[0].toInt()
+  }
+
   var count = 0
-  var last: Int = -1
+  val lineList = arrayListOf<Int>()
 
   File("input.txt").forEachLine {
     val num = it.toInt()
+    lineList.add(0, num)
 
-    if (last >= 0 && num > last) {
+    if (lineList.size > windowSize && lineList.get(windowSize) < lineList.get(0)) {
       count++
     }
-
-    last = num
   }
 
   println(count)
