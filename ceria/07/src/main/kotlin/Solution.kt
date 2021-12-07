6c474b66033dd984a728599f38fed2e8202523cd
cmitchell <ceria.mitchell@gmail.com>
2021-12-07 07:29:15 -0500

    Day 7

---
+++ ceria/07/src/main/kotlin/Solution.kt
@@ -0,0 +1,46 @@
+import java.io.File;
+import kotlin.math.abs;
+
+fun main(args : Array<String>) {
+    val input = File(args.first()).readLines().first().split(",").map { it.toInt() }
+    println("Solution 1: ${solution1(input)}")
+    println("Solution 2: ${solution2(input)}")
+}
+
+private fun solution1(input: List<Int>) :Int {
+    val xPosCounts = input.groupingBy{ it }.eachCount()
+    val calculateFuelFor = xPosCounts.keys
+    val fuels = mutableMapOf<Int, Int>()
+
+    for (possibleXValue in calculateFuelFor) {
+        var fuel = 0
+
+        for (crabPos in input) {
+            fuel += abs(crabPos - possibleXValue)
+        }
+        fuels.put(possibleXValue, fuel)
+    }
+
+    return fuels.values.minOrNull() ?: 0
+}
+    
+private fun solution2(input: List<Int>) :Int {
+    val xPosCounts = input.groupingBy{ it }.eachCount()
+    val calculateFuelFor = xPosCounts.keys
+    val fuels = mutableMapOf<Int, Int>()
+
+    // Have to consider non-listed positions, so the entire range between the min and max
+    for (possibleXValue in calculateFuelFor.minOrNull()!!..calculateFuelFor.maxOrNull()!! ) {
+        var fuel = 0
+
+        for (crabPos in input) {
+            var distance =  abs(crabPos - possibleXValue)
+            for (n in 0..distance) {
+                fuel += n
+            }
+        }
+        fuels.put(possibleXValue, fuel)
+    }
+
+    return fuels.values.minOrNull() ?: 0
+}   
