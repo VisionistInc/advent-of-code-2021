ba96a984624fb3b68e3c3e9c72a251db4512db65
Briforce <bparker222@gmail.com>
2021-12-07 13:39:26 -0500

    07 i am happier with this

--- baparker/07/main.kt
+++ baparker/07/main.kt
@@ -1,16 +1,34 @@
 import java.io.File
 import kotlin.math.abs
+import kotlin.math.ceil
+import kotlin.math.floor
+
+fun getDist(p1: Int, p2: Int): Int {
+    return abs(p1 - p2)
+}
+
+fun getTriangularNum(num: Int): Int {
+    return num * (num + 1) / 2
+}
+
+fun getSmallestSumOfFuelCost(list: List<Int>, center: Double): Int {
+    var floorSum = 0
+    var ceilSum = 0
+    list.forEach({ pos ->
+        val floorCenter = floor(center).toInt()
+        floorSum += getTriangularNum(getDist(pos, floorCenter))
+        val ceilCenter = ceil(center).toInt()
+        ceilSum += getTriangularNum(getDist(pos, ceilCenter))
+    })
+    return minOf(floorSum, ceilSum)
+}
 
 fun main() {
     File("input.txt").forEachLine {
         val positions = it.split(",").map({ pos -> pos.toInt() }).sorted()
         val splitPos = positions.chunked(positions.size / 2)
         println(positions.sumOf({ pos -> abs(pos - splitPos[1][0]) }))
-        val bestWeightedPos = (positions.sum() / positions.size)
-        println(
-                positions.sumOf({ pos ->
-                    abs(pos - bestWeightedPos) * (abs(pos - bestWeightedPos) + 1) / 2
-                })
-        )
+        val bestWeightedPos = (positions.sum().toDouble() / positions.size)
+        println(getSmallestSumOfFuelCost(positions, bestWeightedPos))
     }
 }
