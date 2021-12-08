eb56a29326f155aa1a9dba6daae3a2fd35a38b3d
Briforce <bparker222@gmail.com>
2021-12-08 12:53:06 -0500

    08 is done fml

--- baparker/08/main.kt
+++ baparker/08/main.kt
@@ -1,50 +1,77 @@
 import java.io.File
 
-val CODE_MAP_5 =
-        mapOf(
-                "b" to 2,
-                "g" to 3,
-                "e" to 5,
-        )
-
-val CODE_MAP_6 = mapOf("f" to 0, "a" to 6, "e" to 9)
-
 fun brokenTrash1() {
-    val counter = mutableListOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    var counter = 0
     File("input.txt").forEachLine {
-        it.split("|")[1]
+        it.split(" | ")[1]
                 .split(" ")
                 .forEach({
                     when (it.length) {
-                        2 -> counter.set(0, counter.get(0).inc())
-                        3 -> counter.set(1, counter.get(1).inc())
-                        4 -> counter.set(2, counter.get(2).inc())
-                        7 -> counter.set(3, counter.get(3).inc())
+                        2, 3, 4, 7 -> counter++
                     }
                 })
     }
-    println(counter.sum())
+    println(counter)
 }
 
 fun brokenTrash2() {
     var counter = 0
     File("input.txt").forEachLine {
-        var codeString = ""
+        var codeList: MutableList<List<Char>> = MutableList(10) { listOf() }
+        val input = it.split(" | ")
 
-        it.split(" | ")[1]
-                .split(" ")
-                .forEach({
-                    println(it.toCharArray().sorted().joinToString(separator = ""))
-                    when (it.length) {
-                        2 -> codeString += 1
-                        3 -> codeString += 7
-                        4 -> codeString += 4
-                        5 -> codeString += 
-                        7 -> codeString += 8
-                        else -> codeString += NON_UNIQUE_CODE_MAP.find()
-                    }
+        val patterns = input[0].split(" ")
+        val fives: MutableList<String> = mutableListOf()
+        val sixes: MutableList<String> = mutableListOf()
+
+        // Set the freebies and sort the lengths of 5 and 6
+        patterns.forEach({
+            when (it.length) {
+                2 -> codeList.set(1, it.toCharArray().sorted())
+                3 -> codeList.set(7, it.toCharArray().sorted())
+                4 -> codeList.set(4, it.toCharArray().sorted())
+                5 -> fives.add(it)
+                6 -> sixes.add(it)
+                7 -> codeList.set(8, it.toCharArray().sorted())
+            }
+        })
+        // Figure out the codes of length 6
+        sixes.forEach({ sixCode ->
+            val sixCodeAsCharList = sixCode.toCharArray().sorted()
+            if (codeList.get(4).intersect(sixCodeAsCharList).size == codeList.get(4).size) {
+                codeList.set(9, sixCodeAsCharList)
+            } else if (codeList.get(7).intersect(sixCodeAsCharList).size == codeList.get(7).size) {
+                codeList.set(0, sixCodeAsCharList)
+            } else {
+                codeList.set(6, sixCodeAsCharList)
+            }
+        })
+
+        // Figure out the codes of length 5
+        fives.forEach({ fiveCode ->
+            val fiveCodeAsCharList = fiveCode.toCharArray().sorted()
+            if (codeList.get(7).intersect(fiveCodeAsCharList).size == codeList.get(7).size) {
+                codeList.set(3, fiveCodeAsCharList)
+            } else if (codeList.get(6).intersect(fiveCodeAsCharList).size == fiveCodeAsCharList.size
+            ) {
+                codeList.set(5, fiveCodeAsCharList)
+            } else {
+                codeList.set(2, fiveCodeAsCharList)
+            }
+        })
+
+        val codeListAsStrings = codeList.map({ code -> code.joinToString(separator = "") })
+
+        var outputString = ""
+        input[1].split(" ")
+                .forEach({ output ->
+                    outputString +=
+                            codeListAsStrings.indexOf(
+                                    output.toCharArray().sorted().joinToString(separator = "")
+                            )
                 })
-        counter += codeString.toInt()
+
+        counter += outputString.toInt()
     }
     println(counter)
 }
