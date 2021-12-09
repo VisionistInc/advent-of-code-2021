74448a08e1e6eb8d33496896cec830d980c9ed29
cmitchell <ceria.mitchell@gmail.com>
2021-12-09 13:35:05 -0500

    Day 9 cleaned up.

--- ceria/09/src/main/kotlin/Solution.kt
+++ ceria/09/src/main/kotlin/Solution.kt
@@ -54,7 +54,7 @@ private fun solution1() :Int {
 }
 
 private fun solution2() :Int {
-    var basinPoints = mutableListOf<Pair<Int, Int>>()
+    var basinSizes = mutableListOf<Int>()
     for (rowIdx in heights.indices) {
         val row = heights[rowIdx]
 
@@ -82,39 +82,34 @@ private fun solution2() :Int {
             }
 
             // if we made it this far, this is a basin, compute it's size
-            basinPoints.add(Pair<Int, Int>(idx, rowIdx))
-        }
-    }
-
-    var basinSizes = mutableListOf<Int>()
-    for (point in basinPoints) {
-        var basinBounds = mutableSetOf<Pair<Int, Int>>(point)
-        var allBounded = false
+            var basinBounds = mutableSetOf<Pair<Int, Int>>(Pair<Int, Int>(idx, rowIdx))
+            var allBounded = false
         
-        while (!allBounded) {
-            var newPoints = mutableSetOf<Pair<Int, Int>>()
-            for (bp in basinBounds) {
-                newPoints.addAll(checkForBounds(bp, basinBounds))
-            }
-            basinBounds.addAll(newPoints)
+            while (!allBounded) {
+                var newPoints = mutableSetOf<Pair<Int, Int>>()
+                for (bp in basinBounds) {
+                    newPoints.addAll(checkForBounds(bp, basinBounds))
+                }
+                basinBounds.addAll(newPoints)
 
-            if (newPoints.isEmpty()) {
-                allBounded = true
+                if (newPoints.isEmpty()) {
+                    allBounded = true
+                }
             }
+
+            basinSizes.add(basinBounds.size)
         }
-
-        basinSizes.add(basinBounds.size)
     }
 
-    // take the 3 largest
     basinSizes.sort()
+    // return the product of the 3 largest values
     return basinSizes.takeLast(3).reduce{ acc, i ->  acc * i }
 }   
 
 private fun checkForBounds(p: Pair<Int, Int>, basinPoints: Set<Pair<Int, Int>>) :Set<Pair<Int, Int>> {
      var newPoints = mutableSetOf<Pair<Int, Int>>()
 
-    // compare to the point to the left - if it's not 9, and not already in points, add it to points and recurse
+    // compare to the point to the left - if it's not 9, and not already in newPoints, add it to newPoints
     if (p.first > 0 && heights[p.second][p.first - 1] != 9) {
        val newPoint = Pair<Int, Int>(p.first - 1, p.second)
         if (!basinPoints.contains(newPoint)) {
@@ -122,7 +117,7 @@ private fun checkForBounds(p: Pair<Int, Int>, basinPoints: Set<Pair<Int, Int>>) 
         }
     }
 
-    // compare to the point to the right - if it's not 9, and not already in points, add it to points and recurse
+    // compare to the point to the right - if it's not 9, and not already in newPoints, add it to newPoints
     if (p.first < heights[0].size - 1 && heights[p.second][p.first + 1] != 9) {
         val newPoint = Pair<Int, Int>(p.first + 1, p.second)
         if (!basinPoints.contains(newPoint)) {
@@ -130,7 +125,7 @@ private fun checkForBounds(p: Pair<Int, Int>, basinPoints: Set<Pair<Int, Int>>) 
         }
     }
 
-    // compare to the point above - if it's not 9, and not already in points, add it to points and recurse
+    // compare to the point above - if it's not 9, and not already in newPoints, add it to newPoints
     if (p.second > 0 && heights[p.second - 1][p.first] != 9) {
         val newPoint = Pair<Int, Int>(p.first, p.second - 1)
         if (!basinPoints.contains(newPoint)) {
@@ -138,7 +133,7 @@ private fun checkForBounds(p: Pair<Int, Int>, basinPoints: Set<Pair<Int, Int>>) 
         }
     }
 
-    // compare to the point above - if it's not 9, and not already in points, add it to points and recurse
+    // compare to the point above - if it's not 9, and not already in newPoints, add it to newPoints
     if (p.second < heights.size -1 && heights[p.second + 1][p.first] != 9) {
         val newPoint = Pair<Int, Int>(p.first, p.second + 1)
         if (!basinPoints.contains(newPoint)) {
