e0dde9fac37a80ab01fa60572de46433fa63fe7c
cmitchell <ceria.mitchell@gmail.com>
2021-12-10 07:18:36 -0500

    Day 10

--- ceria/10/src/main/kotlin/Solution.kt
+++ ceria/10/src/main/kotlin/Solution.kt
@@ -7,17 +7,24 @@ val matches = mapOf<Char, Char>(
     '}' to '{', 
     '>' to '<'
 )
-val leftChars = setOf<Char>('(', '[', '{', '<')
+val leftChars = matches.values.toSet()
 val corruptedValues = mapOf<Char, Int>(
     ')' to 3, 
     ']' to 57,
     '}' to 1197, 
     '>' to 25137
 )
+val scoreValues = mapOf<Char, Int>(
+    ')' to 1, 
+    ']' to 2,
+    '}' to 3, 
+    '>' to 4
+)
 
 fun main(args : Array<String>) {
     val input = File(args.first()).readLines()
     println("Solution 1: " + solution1(input))
+    println("Solution 1: " + solution2(input))
     
     // CANNOT USE STRING INTERPLATION BECAUSE OF SPECIAL CHARACTERS IN THE INPUT
     // println("Solution 1: ${solution1(input)}")
@@ -48,6 +55,54 @@ private fun solution1(input: List<String>) :Int {
     return sum
 }
 
-// private fun solution2(input: List<String>) :Int {
-//    return 0
-// }
+private fun solution2(input: List<String>) :Long {
+    val reversedMatches = matches.entries.associate { (key, value) -> value to key }
+    var filteredInput = input.toMutableList()
+
+    for (line in input) {
+        val syntax = Stack<Char>()
+        for (c in line) {
+            if (c in leftChars) {   
+                syntax.push(c)
+            } else {
+                val shouldMatch = syntax.pop()
+                if (!shouldMatch.equals(matches.get(c))) {
+                    filteredInput.remove(line)
+                    break
+                }
+            }
+        }
+    }
+
+    val completionStr = mutableListOf<String>()
+    for (line in filteredInput) {
+        val syntax = Stack<Char>()
+        for (c in line) {
+            if (c in leftChars) {   
+                syntax.push(c)
+            } else {
+                syntax.pop()
+            }
+        }
+
+        var completion = ""
+        while (!syntax.isEmpty()) {
+            val match = syntax.pop()
+            completion += reversedMatches.get(match)
+        }
+        completionStr.add(completion)
+    }
+
+    val scores = mutableListOf<Long>()
+    for (completion in completionStr) {
+        var score = 0L
+        for (c in completion) {
+            score = (score * 5) + scoreValues.get(c)!!
+        }
+        scores.add(score)
+    }
+
+    scores.sort()
+    val middle = scores.size/2
+    return scores.get(middle)  // since the list is 0 based, dividing the size by 2 gives the middle index.
+}
