a7c10c0efff4f45a6041a63da35adf17dd23e280
Briforce <bparker222@gmail.com>
2021-12-13 12:05:25 -0500

    improved 13

--- baparker/13/main.kt
+++ baparker/13/main.kt
@@ -3,11 +3,11 @@ import java.io.File
 fun main() {
     val dotList: MutableList<Pair<Int, Int>> = mutableListOf()
     val foldList: MutableList<Pair<Int, Int>> = mutableListOf()
-    var highestX = 0
-    var highestY = 0
+
     File("input.txt").forEachLine {
-        if (it.startsWith("fold along")) {
-            val fold = it.split(" ")[2].split("=")
+        val inputArray = it.split(" ")
+        if (inputArray.size > 1) {
+            val fold = inputArray[2].split("=")
             if (fold[0] == "x") {
                 foldList.add(Pair(fold[1].toInt(), 0))
             } else if (fold[0] == "y") {
@@ -16,12 +16,6 @@ fun main() {
         } else {
             val coordinates = it.split(",")
             if (coordinates.size == 2) {
-                if (coordinates[0].toInt() > highestX) {
-                    highestX = coordinates[0].toInt()
-                }
-                if (coordinates[1].toInt() > highestY) {
-                    highestY = coordinates[1].toInt()
-                }
                 dotList.add(Pair(coordinates[0].toInt(), coordinates[1].toInt()))
             }
         }
@@ -43,17 +37,13 @@ fun main() {
                 }
             }
         }
+        print(" -> " + dotList.toSet().size)
     }
+    println()
 
-    // Probably a much better way to get these numbers for the sizes and loops but I am lazy. To be
-    // improved I suppose
-    val board: List<MutableList<Char>> = List(40) { MutableList(10) { '.' } }
-
-    dotList.toSet().forEach { board.get(it.first).set(it.second, '#') }
-
-    for (i in 0..5) {
-        for (j in 0..38) {
-            print(board.get(j).get(i))
+    for (i in 0..dotList.maxOf { it.second }) {
+        for (j in 0..dotList.maxOf { it.first }) {
+            if (dotList.contains(Pair(j, i))) print("#") else print(".")
         }
         println()
     }
